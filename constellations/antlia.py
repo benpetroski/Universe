f364680094450e6caf18996a6b37f705f19d83af
benpetroski <ben@benpetroski.com>
2014-06-18 00:41:31 -0700

    Icon window. Icon doesn't appear. Probably need to change to local path.

--- constellations/antlia.py
+++ constellations/antlia.py
@@ -3,6 +3,25 @@ Created on Jun 18, 2014
 
 @author: Ben Petroski
 '''
+import sys
+from PyQt4 import QtGui
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QtGui.QIcon('web.png'))
+        
+        self.show()
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    pass
+    main()
