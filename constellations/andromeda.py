9cb9ba9df7f2620b72102cd7e10e8bca2873cedd
benpetroski <ben@benpetroski.com>
2014-06-18 00:07:09 -0700

    Committing antlia.py -> second script in PyQt4 tut

--- constellations/andromeda.py
+++ constellations/andromeda.py
@@ -6,6 +6,16 @@ Created on Jun 16, 2014
 import sys
 from PyQt4 import QtCore, QtGui
  
-if __name__ == '__main__':
-    print 'radio'
+def main():
+    app = QtGui.QApplication(sys.argv)
      
+    w = QtGui.QWidget()
+    w.resize(250,150)
+    w.move(300,300)
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    sys.exit(app.exec_())
+ 
+if __name__ == '__main__':
+    main()
---
+++ constellations/antlia.py
@@ -0,0 +1,8 @@
+'''
+Created on Jun 18, 2014
+
+@author: Ben Petroski
+'''
+
+if __name__ == '__main__':
+    pass
