99bb31a10463372764cc5c8fbfbd8acd15e61c63
Ben Petroski <ben@benpetroski.com>
2014-06-19 09:56:29 -0700

    Final commit of aquarius. Functional.

--- constellations/aquarius.py
+++ constellations/aquarius.py
@@ -2,7 +2,7 @@
 @author: benpetroski
 '''
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 class Example(QtGui.QWidget):
     def __init__(self):
@@ -10,13 +10,10 @@ class Example(QtGui.QWidget):
         self.initUI()
         
     def initUI(self):
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif',10))
-        self.setToolTip('This is a <b>QWidget</b> widget.')
-        
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget.')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+        qbtn = QtGui.QPushButton('Quit', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)
         
         self.setGeometry(300,300,250,150)
         self.setWindowTitle('Tooltips')        
