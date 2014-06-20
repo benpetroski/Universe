e86f8deaf9a9a1eb9750849596b470e5ee374040
Ben Petroski <ben@benpetroski.com>
2014-06-20 00:21:16 -0700

    Initial commit of aquila.py -> message box prompt when quit.

---
+++ constellations/aquila.py
@@ -0,0 +1,30 @@
+'''
+@author: benpetroski
+'''
+import sys
+from PyQt4 import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+        
+    def initUI(self):        
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle('Message Box')        
+        self.show()
+        
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message', 'Are you sure you want to quit?', QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()        
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
